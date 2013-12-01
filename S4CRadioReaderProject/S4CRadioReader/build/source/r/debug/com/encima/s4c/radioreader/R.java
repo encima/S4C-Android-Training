0beb23f927d0a0d018b096400a19f9b98a7fcdb8
Christopher Gwilliams <encima@gmail.com>
2013-12-01 21:13:44 +0000

    JSON app

--- S4CRadioReaderProject/S4CRadioReader/build/source/r/debug/com/encima/s4c/radioreader/R.java
+++ S4CRadioReaderProject/S4CRadioReader/build/source/r/debug/com/encima/s4c/radioreader/R.java
@@ -21,11 +21,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int listView=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int api=0x7f080004;
+        public static final int getdata=0x7f080001;
+        public static final int list=0x7f080000;
+        public static final int name=0x7f080003;
+        public static final int vers=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list_v=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -36,7 +41,8 @@ public final class R {
         public static final int action_settings=0x7f050000;
         public static final int app_name=0x7f050001;
         public static final int hello_world=0x7f050002;
-        public static final int radio_url=0x7f050003;
+        public static final int json_url=0x7f050003;
+        public static final int radio_url=0x7f050004;
     }
     public static final class style {
         /**  From: file:/Users/encima/Development/android/S4C/S4CRadioReaderProject/S4CRadioReader/src/main/res/values/styles.xml 
